21c3ab88bfb84599958331d5c9f18d9f5e91afd9
zhiqiangong <23008087@hope.ac.uk>
2024-10-12 13:52:58 +0100

    Add files via upload

---
+++ custom_properties_appointment/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Calendar Extension with Rental Properties',
+    'version': '1.0',
+    'summary': 'Adds rental properties tab to the calendar event form.',
+    'category': 'Calendar',
+    'author': 'Eco-clic',
+    'depends': ['calendar', 'alquileres'],
+    'data': [
+        'views/calendar_event_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
---
+++ custom_properties_appointment/models/calendar_event.py
@@ -0,0 +1,6 @@
+from odoo import models, fields
+
+class CalendarEvent(models.Model):
+    _inherit = 'calendar.event'
+
+    rental_property_id = fields.Many2one('rental.property', string='Rental Property')
