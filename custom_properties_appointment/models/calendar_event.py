9b3fcddb22b2b8401b899c7d6aa7ad09d42b7ef9
zhiqiangong <23008087@hope.ac.uk>
2024-10-14 12:07:20 +0100

    Add files via upload

--- custom_properties_appointment/models/calendar_event.py
+++ custom_properties_appointment/models/calendar_event.py
@@ -17,3 +17,17 @@ class CalendarEvent(models.Model):
             ])
             if overlapping_events:
                 raise ValidationError("You cannot have overlapping events for the same rental property.")
+
+class RentalProperty(models.Model):
+    _name = 'rental.property'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = 'Property Management'
+
+    # Otros campos...
+
+    def action_view_appointments(self):
+        self.ensure_one()
+        action = self.env.ref('calendar.action_calendar_event').read()[0]
+        action['domain'] = [('rental_property_id', '=', self.id)]
+        action['context'] = {'default_rental_property_id': self.id}
+        return action
