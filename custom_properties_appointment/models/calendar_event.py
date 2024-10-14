149241fb97aa9cf5cf99e39a4c73effc6841aa66
zhiqiangong <23008087@hope.ac.uk>
2024-10-14 11:56:41 +0100

    Add files via upload

--- custom_properties_appointment/models/calendar_event.py
+++ custom_properties_appointment/models/calendar_event.py
@@ -17,13 +17,3 @@ class CalendarEvent(models.Model):
             ])
             if overlapping_events:
                 raise ValidationError("You cannot have overlapping events for the same rental property.")
-
-    @api.model
-    def check_event_conflict(self, event_id, start, stop, rental_property_id):
-        overlapping_events = self.search([
-            ('id', '!=', event_id),
-            ('rental_property_id', '=', rental_property_id),
-            ('start', '<', stop),
-            ('stop', '>', start),
-        ])
-        return {'conflict': bool(overlapping_events)}
