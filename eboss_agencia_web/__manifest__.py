0c05c66efa901777c354366030bae74831f64a3a
zhiqiangong <23008087@hope.ac.uk>
2024-11-11 14:59:11 +0000

    Add files via upload

--- eboss_agencia_web/__manifest__.py
+++ eboss_agencia_web/__manifest__.py
@@ -9,22 +9,24 @@
         "base",
         "website",
     ],
+
     "data": [
         "views/homepage.xml",
-        "views/layout.xml",
+        "views/layout.xml",     
         "views/alquileres.xml",       
         "views/contacto.xml",
         "views/faqs.xml",
         "views/nosotros.xml",
-        "views/css_loader.xml",
-  
     ],
+
     "assets": {
         'web.assets_frontend': [
             'eboss_agencia_web/static/src/css/estilos.css',
             'eboss_agencia_web/static/src/img/logo.png',
+            'eboss_agencia_web/static/src/img/imagen.png',
         ],
     },
+
     "installable": True,
     "application": False,
 }
